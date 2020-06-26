def21f356983db41c7af85fbf2a7606467139e5a
samczsun <github@samczsun.com>
2020-06-26 16:16:54 -0400

    some upgrades to the install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,18 +1,56 @@
 #!/bin/bash
 
+PARAMS=""
+while (( "$#" )); do
+  case "$1" in
+    --force)
+        export FORCE=1
+        shift
+        ;;
+    --after)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        export SOLC_AFTER=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    --before)
+      if [ -n "$2" ] && [ ${2:0:1} != "-" ]; then
+        export SOLC_BEFORE=$2
+        shift 2
+      else
+        echo "Error: Argument for $1 is missing" >&2
+        exit 1
+      fi
+      ;;
+    -*|--*=) # unsupported flags
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      PARAMS="$PARAMS $1"
+      shift
+      ;;
+  esac
+done
+# set positional arguments in their proper place
+eval set -- "$PARAMS"
+
 OS=$(uname)
 
 #
 # Install locally on Linux
 
-if [ $OS = "Linux" ]; then
+if [[ ${OS} = "Linux" ]]; then
     echo "Detected Linux"
 
     REPO_SCRIPTDIR=`dirname "$0"`
     cd $REPO_SCRIPTDIR
 
     if [ ! -f ./install_linux.sh ]; then
-        echo "Cannot find Linux installation script. If you are installing via Docker - it is unecessary on Linux, instead refer to Linux installation instructions in README.md"
+        echo "Cannot find Linux installation script. If you are installing via Docker - it is unnecessary on Linux, instead refer to Linux installation instructions in README.md"
         exit 1
     fi
 
