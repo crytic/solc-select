38b5707b3bee179e3e66abdd91944ba2ae010d82
Artur Cygan <arczicygan@gmail.com>
2020-12-28 23:36:31 +0100

    Fix bugs

--- bin-experimental/solc_select.py
+++ bin-experimental/solc_select.py
@@ -10,15 +10,17 @@ os.makedirs(artifacts_dir, exist_ok=True)
 def current_version():
     version = os.environ.get('SOLC_VERSION')
     source = 'SOLC_VERSION'
-    if not version:
+    if version:
+        if version not in get_installed_versions():
+            print(f"Version '{version}' not installed (set by {source}). Run `solc-select install {version}`.")
+            exit(1)
+    else:
         source = f"{solc_select_dir}/global-version"
         if os.path.isfile(source):
             with open(source) as f: version = f.read()
         else:
-            print('Run `solc-select use VERSION` or set SOLC_VERSION environment variable')
-            sys.exit(1)
-    if version not in get_installed_versions():
-        return None
+            print('No solc version set. Run `solc-select use VERSION` or set SOLC_VERSION environment variable.')
+            return None
     return (version, source)
 
 def get_installed_versions():
