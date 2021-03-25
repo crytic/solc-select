87015a9bfaf25565ca61c0a5a71ab1a96e0e1cbe
Natalie Chin <natalie.chin@trailofbits.com>
2021-03-25 14:51:15 -0400

    Applied black changes

--- solc_select/__main__.py
+++ solc_select/__main__.py
@@ -1,8 +1,17 @@
 import argparse
 import os
 import sys
-from .solc_select import valid_install_arg, valid_version, get_installable_versions, install_artifacts, \
-    switch_global_version, current_version, installed_versions, artifacts_dir
+from .solc_select import (
+    valid_install_arg,
+    valid_version,
+    get_installable_versions,
+    install_artifacts,
+    switch_global_version,
+    current_version,
+    installed_versions,
+    artifacts_dir,
+)
+
 
 def solc_select():
     INSTALL_VERSIONS = "INSTALL_VERSIONS"
