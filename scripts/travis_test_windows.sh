165846fc7de0e676c069d9e75913d8c4223dd775
Natalie Chin <natalie.chin@trailofbits.com>
2021-07-07 10:59:48 -0400

    Moved linux, macos, windows ci tests to sh files

---
+++ scripts/travis_test_windows.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+sudo python3 setup.py install
+solc-select install all
+
+solc-select use 0.4.5
+if [ $? != "Switched global version to 0.4.5" ]; then
+  echo "Windows minimum version failed"
+  exit 255
+fi
+
+solc-select use 0.8.6
+if [ $? != "Switched global version to 0.8.6" ]; then
+  echo "Windows maximum version failed"
+  exit 255
+fi
