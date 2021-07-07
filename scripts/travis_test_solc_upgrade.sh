69c30dd3d66c31bdda6aac581ea4d9a44b4f6e9c
Natalie Chin <natalie.chin@trailofbits.com>
2021-07-07 11:12:30 -0400

    Moved solc-select installation to main ci and out of test scripts

--- scripts/travis_test_solc_upgrade.sh
+++ scripts/travis_test_solc_upgrade.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 ### Install old version of solc
-pip3 install solc-select
+sudo pip3 uninstall solc-select
+sudo pip3 install solc-select
 old_solc_version=$(solc --version)
 solc-select install 0.4.6 0.5.0 0.6.12 0.7.3 0.8.3
 all_old_versions=$(solc-select versions)
 
 ### Install new version of solc
-python3 setup.py develop
+sudo python3 setup.py develop
 new_solc_version=$(solc --version)
 all_new_versions=$(solc-select versions)
 
