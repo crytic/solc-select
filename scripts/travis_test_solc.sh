ea464c37909e4bac694737b69da2462ed981f871
Natalie Chin <natalie.chin@trailofbits.com>
2021-07-08 10:35:34 -0400

    Added tests for solc 0.8.0

--- scripts/travis_test_solc.sh
+++ scripts/travis_test_solc.sh
@@ -31,3 +31,26 @@ if [[ "$execute" != *"Error: Functions are not allowed to have the same name as 
   exit 255
 fi
 echo "SUCCESS: solc045_fail_compile"
+
+solc-select use 0.8.0  &> /dev/null
+solc ./scripts/solidity_tests/solc080_success.sol
+
+if [[ $? != 0 ]]; then
+  echo "FAILED: solc080_success" $?
+  exit 255
+fi
+echo "SUCCESS: solc080_success"
+
+execute=$(solc ./scripts/solidity_tests/solc080_success_warning.sol 2>&1)
+if [[ "$execute" != *"Warning: Function state mutability can be restricted to pure"* ]]; then
+  echo "FAILED: solc080_success_warning"
+  exit 255
+fi
+echo "SUCCESS: solc080_success_warning"
+
+execute=$(solc ./scripts/solidity_tests/solc080_fail_compile.sol 2>&1)
+if [[ "$execute" != *"Error: Explicit type conversion not allowed"* ]]; then
+  echo "FAILED: solc080_fail_compile"
+  exit 255
+fi
+echo "SUCCESS: solc045_fail_compile"
