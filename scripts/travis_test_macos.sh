df273ed91911b96bb9340a23b7873b90866869cd
Natalie Chin <natalie.chin@trailofbits.com>
2021-07-09 14:02:16 -0400

    Added dynamic latestRelease

--- scripts/travis_test_macos.sh
+++ scripts/travis_test_macos.sh
@@ -2,17 +2,30 @@
 
 use_version=$(solc-select use 0.3.6)
 if [[ $use_version != "Switched global version to 0.3.6"* ]]; then
-  echo "OS X FAILED: minimum version"
+  echo "OS X FAILED: set minimum version"
   exit 255
 fi
-echo "OS X SUCCESS: minimum version"
+echo "OS X SUCCESS: set minimum version"
 
-use_version=$(solc-select use 0.8.6)
-if [[ $use_version != "Switched global version to 0.8.6" ]]; then
-  echo "OS X FAILED: maximum version"
+latest_release=$(curl https://binaries.soliditylang.org/macosx-amd64/list.json |
+  python3 -c "import sys,json; print(json.load(sys.stdin)['latestRelease'])")
+use_version=$(solc-select use "$latest_release")
+if [[ $use_version != "Switched global version to $latest_release" ]]; then
+  echo "OS X FAILED: set maximum version"
   exit 255
 fi
-echo "OS X SUCCESS: maximum version"
+echo "OS X SUCCESS: set maximum version"
 
-solc-select use 0.4.5  &> /dev/null
-solc scripts/solidity_tests/solc045_success.sol
+use_version=$(solc-select use 0.3.5 2>&1)
+if [[ $use_version != *"Invalid version - only solc versions above '0.3.6' are available"* ]]; then
+  echo "OS X FAILED: version too low"
+  exit 255
+fi
+echo "OS X SUCCESS: version too low"
+
+use_version=$(solc-select use 0.100.8 2>&1)
+if [[ $use_version != *"Invalid version '$latest_release' is the latest available version"* ]]; then
+  echo "OS X FAILED: version too high"
+  exit 255
+fi
+echo "OS X SUCCESS: version too high"
--- scripts/travis_test_windows.sh
+++ scripts/travis_test_windows.sh
@@ -7,9 +7,25 @@ if [[ $use_version != "Switched global version to 0.4.5"* ]]; then
 fi
 echo "WINDOWS SUCCESS: minimum version"
 
-use_version=$(solc-select use 0.8.6)
-if [[ $use_version != "Switched global version to 0.8.6"* ]]; then
+latest_release=$(curl https://binaries.soliditylang.org/linux-amd64/list.json |
+  python3 -c "import sys,json; print(json.load(sys.stdin)['latestRelease'])")
+use_version=$(solc-select use "$latest_release")
+If [[ $use_version != "Switched global version to $latest_release" ]]; then
   echo "WINDOWS FAILED: maximum version"
   exit 255
 fi
 echo "WINDOWS SUCCESS: maximum version"
+
+use_version=$(solc-select use 0.3.9 2>&1)
+if [[ $use_version != *"Invalid version - only solc versions above '0.4.5' are available"* ]]; then
+  echo "WINDOWS FAILED: version too low"
+  exit 255
+fi
+echo "WINDOWS SUCCESS: version too low"
+
+use_version=$(solc-select use 0.100.8 2>&1)
+if [[ $use_version != *"Invalid version '$latest_release' is the latest available version"* ]]; then
+  echo "WINDOWS FAILED: version too high"
+  exit 255
+fi
+echo "WINDOWS SUCCESS: version too high"
