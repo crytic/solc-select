97fe57eeb098fada385c563b87ae7b99e5357d3d
Nat Chin <natalie.chin@trailofbits.com>
2022-10-10 15:09:50 +0000

    Bump `setup.py` to v2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description="Manage multiple Solidity compiler versions.",
     url="https://github.com/crytic/solc-select",
     author="Trail of Bits",
-    version="1.0.0.b1",
+    version="2.0.0.0",
     packages=find_packages(),
     python_requires=">=3.6",
     license="AGPL-3.0",
