ff8daa1708ec25f20b50538ef973ca99f9bab197
Nat Chin <natalie.chin@trailofbits.com>
2023-02-02 20:24:52 -0500

    Hotfixes 0.8.18 to pull from crytic/solc (#159)
    * Real hot fix
    
    * updated return link
    
    * Linting errors

--- solc_select/constants.py
+++ solc_select/constants.py
@@ -20,3 +20,9 @@ MACOSX_AMD64 = "macosx-amd64"
 WINDOWS_AMD64 = "windows-amd64"
 
 EARLIEST_RELEASE = {"macosx-amd64": "0.3.6", "linux-amd64": "0.4.0", "windows-amd64": "0.4.5"}
+
+# URL
+CRYTIC_SOLC_ARTIFACTS = "https://raw.githubusercontent.com/crytic/solc/master/linux/amd64/"
+CRYTIC_SOLC_JSON = (
+    "https://raw.githubusercontent.com/crytic/solc/new-list-json/linux/amd64/list.json"
+)
